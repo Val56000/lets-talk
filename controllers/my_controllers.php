35d501832357d3fbd86f65cf33ec3915301731d5
Val56000 <valentin.harnay2021@campus-eni.fr>
2022-04-29 17:37:18 +0200

    ajout my_controllers qui interroge 'Models'

---
+++ controllers/my_controllers.php
@@ -0,0 +1,7 @@
+<?php
+include_once 'C:\xampp\htdocs\lets-talk\models\all_commentaires.php';
+
+
+//fonction permetttant de récupérer le nom, prenom et role où le rôle est = administrateur
+$getAllCommentaires = getAllCommentaires();
+?>
