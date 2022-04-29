ee71ad1424cb184e877832fcda7edf8a09e5e2e9
Val56000 <valentin.harnay2021@campus-eni.fr>
2022-04-29 18:07:17 +0200

    Modif suppression.js

--- javascript/suppression.js
+++ javascript/suppression.js
@@ -2,8 +2,8 @@ let suppression = document.querySelector("#sup");
 suppression.addEventListener("click", () => {
     var retVal = confirm("Etes-vous sûr de vouloir supprimer le compte ?");
     if( retVal == true ){
-    //ici vous pouvez mettre votre en cas de confirmation
-       //moi j'affiche une boite d'alerte
+    
+       //j'affiche une boite d'alerte
           alert("Compte supprimé avec succès !");
           window.location.replace("/lets-talk/models/suppression.php");
                return true;
